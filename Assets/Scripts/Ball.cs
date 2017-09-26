7f74d666915abde6d560359918e7c762e9779a17
Alex Delude <32208447+alexdelude@users.noreply.github.com>
2017-09-26 15:12:56 -0700

    First commit of Block Breaker Unity project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle; 
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle> ();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			//locks the ball to the paddle until launch
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			//launches the ball when mouse is clicked
+			if (Input.GetMouseButtonDown (0)) {
+				print ("mouse clicked, launch ball");
+				hasStarted = true;
+				this.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision){
+		AudioSource audio = GetComponent<AudioSource> ();
+
+		Vector2 variance = new Vector2 (Random.Range(0f,0.2f), Random.Range(0f,0.2f));
+
+		if (hasStarted == true) {
+			audio.Play ();
+			GetComponent<Rigidbody2D> ().velocity += variance;
+		}
+	
+	}
+
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	public GameObject Smoke;
+
+	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
+
+
+	// Use this for initialization
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) { //keeps track of breakable bricks
+			breakableCount++;  
+		}
+
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+
+		AudioSource audio = GetComponent<AudioSource> ();
+		AudioSource.PlayClipAtPoint (crack, transform.position, 0.8f);
+		if (isBreakable) {
+			HandleHits ();
+		}
+	}
+
+	void HandleHits() {
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+
+		if (timesHit >= maxHits) { //in case we go over max hits in the future with power ups etc
+			breakableCount--;
+			levelManager.BrickDestroyed ();
+	
+			Destroy (gameObject);
+		} else {
+			LoadSprites ();
+		}
+	}
+
+	void SimulateWin (){
+		levelManager.LoadNextLevel ();
+	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
+	}
+		
+
+}
